ff5b8112f36e2fed3192c638045c68017d5f46a9
kurehajime <xiidec@gmail.com>
2022-08-15 23:12:45 +0900

    生成

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import ja_word from './assets/ja/words.json'
+import { Rule } from './Rule'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
+const runes = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎくげこざじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ'.split('')
+const result = Rule.pick4word(runes, ja_word, 999)
+console.log(result)
+
+
+
+
+
+
 
 root.render(<div className='container'>
 </div>)
