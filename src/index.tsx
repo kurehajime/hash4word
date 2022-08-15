3108123e4603ecdef71fdfa18f1235aaa967025c
kurehajime <xiidec@gmail.com>
2022-08-15 21:43:08 +0900

    ガワ

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+)
+
+root.render(<div className='container'>
+</div>)
