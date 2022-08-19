94c826c2bcdcac649e231069a05042e7b533ff3a
kurehajime <xiidec@gmail.com>
2022-08-19 23:44:30 +0900

    ほバー

--- src/compornents/CellElement.tsx
+++ src/compornents/CellElement.tsx
@@ -4,17 +4,24 @@ type Props = {
     cell: Cell
     x: number
     y: number
+    mouseX: number
+    mouseY: number
     cellSize: number
     selected: boolean
 }
 export default function CellElement(props: Props) {
     const cellSize = props.cellSize
+    const x = props.selected ? props.mouseX : props.x
+    const y = props.selected ? props.mouseY : props.y
     return (
         props.cell.enabled ? <g>
             <rect x={props.x} y={props.y} width={cellSize} height={cellSize} stroke='black'
-                fill={props.selected ? 'blue' : 'whitesmoke'} className="easeIn" />
-            <text x={props.x + cellSize / 2} y={props.y + cellSize / 2}
-                textAnchor="middle" dominantBaseline="central" stroke={props.cell.fixed ? "green" : "black"}>{props.cell.Rune}</text>
+                fill='white' className="" />
+            <rect x={x} y={y} width={cellSize} height={cellSize} stroke='black'
+                fill='whitesmoke' className={props.selected ? "" : "easeIn"} />
+            <text x={x + cellSize / 2} y={y + cellSize / 2}
+                textAnchor="middle" dominantBaseline="central" stroke={props.cell.fixed ? "green" : "black"}
+            >{props.cell.Rune}</text>
             {/* <text x={props.x + 15 + cellSize / 2} y={props.y + cellSize / 2}
                 textAnchor="middle" dominantBaseline="central" stroke="red">{props.cell.x + props.cell.y * 9}</text> */}
 
--- src/compornents/FieldElement.tsx
+++ src/compornents/FieldElement.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { Field } from "../models/Field"
 import { Point } from "../models/Point"
 import CellElement from "./CellElement"
@@ -14,34 +15,64 @@ export default function FieldElement(props: Props) {
     const cellSize = props.cellSize
     const size = props.field.size
     const FieldSize = cellSize * size
+    const [mouseX, setMouseX] = React.useState<number>(0)
+    const [mouseY, setMouseY] = React.useState<number>(0)
+
 
     const mouseClick = (e: React.PointerEvent<SVGSVGElement>) => {
         const x = e.nativeEvent.offsetX
         const y = e.nativeEvent.offsetY
+        setMouseX(x)
+        setMouseY(y)
         clicked(x, y)
         e.preventDefault()
     }
-
+    const mouseMove = (e: React.PointerEvent<SVGSVGElement>) => {
+        const x = e.nativeEvent.offsetX
+        const y = e.nativeEvent.offsetY
+        setMouseX(x)
+        setMouseY(y)
+    }
     const clicked = (x: number, y: number) => {
         props.clicked({ x: Math.floor(x / cellSize), y: Math.floor(y / cellSize) })
     }
 
 
-    return (<svg width={FieldSize} height={FieldSize} onClick={mouseClick}   >
+    return (<svg width={FieldSize} height={FieldSize} onClick={mouseClick}
+        onMouseMove={mouseMove} >
         <ScoreElement
             field={props.field}
             cellSize={cellSize}
         />
         {props.field.Cells.map((cell, index) => {
-            const x = cell.x * cellSize
-            const y = cell.y * cellSize
             const selected = props.seleted !== null && props.seleted.x === cell.x && props.seleted.y === cell.y
-
+            if (selected) {
+                return <></>
+            }
             return <CellElement
                 key={index}
                 cell={cell}
-                x={x}
-                y={y}
+                x={cell.x * cellSize}
+                y={cell.y * cellSize}
+                mouseX={0}
+                mouseY={0}
+                cellSize={cellSize}
+                selected={selected}
+            />
+        })
+        }
+        {props.field.Cells.map((cell, index) => {
+            const selected = props.seleted !== null && props.seleted.x === cell.x && props.seleted.y === cell.y
+            if (!selected) {
+                return <></>
+            }
+            return <CellElement
+                key={index}
+                cell={cell}
+                x={cell.x * cellSize}
+                y={cell.y * cellSize}
+                mouseX={mouseX - cellSize / 2}
+                mouseY={mouseY - cellSize / 2}
                 cellSize={cellSize}
                 selected={selected}
             />
