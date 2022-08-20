ca963b9b723f310c1d9042c5f8bf520bed83492b
kurehajime <xiidec@gmail.com>
2022-08-20 23:28:59 +0900

    デザイン調整

--- src/compornents/ScoreElement.tsx
+++ src/compornents/ScoreElement.tsx
@@ -10,14 +10,22 @@ export default function ScoreElement(props: Props) {
     const size = props.field.size
     const FieldSize = cellSize * size
     return (<g width={FieldSize} height={FieldSize}>
-        <text x={(3 + 1.5) * cellSize} y={(0 + 1) * (cellSize / 2)}
-            textAnchor="middle" dominantBaseline="central" fill="#e6edff">{props.field.calc_left()}</text>
-        <text x={(5 + 1.5) * cellSize} y={(0 + 1) * (cellSize / 2)}
-            textAnchor="middle" dominantBaseline="central" fill="#e6edff">{props.field.calc_right()}</text>
-        <text x={(0 + 0.5) * (cellSize)} y={(3 - 0.5) * (cellSize)}
-            textAnchor="middle" dominantBaseline="central" fill="#e6edff">{props.field.calc_top()}</text>
-        <text x={(0 + 0.5) * (cellSize)} y={(5 - 0.5) * (cellSize)}
-            textAnchor="middle" dominantBaseline="central" fill="#e6edff">{props.field.calc_bottom()}</text>
+        <text x={(3 + 1.3) * cellSize} y={(0 + 1) * (cellSize / 2)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>{props.field.calc_left() + ' hit'}</text>
+        <text x={(3 + 1.3) * cellSize} y={(0 + 1.5) * (cellSize / 2)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>▼</text>
+        <text x={(5 + 1.3) * cellSize} y={(0 + 1) * (cellSize / 2)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>{props.field.calc_right() + ' hit'}</text>
+        <text x={(5 + 1.3) * cellSize} y={(0 + 1.5) * (cellSize / 2)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>▼</text>
+        <text x={(0 + 0.5) * (cellSize)} y={(3 - 0.2) * (cellSize)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>{props.field.calc_top() + ' hit'}</text>
+        <text x={(0 + 0.9) * (cellSize)} y={(3 - 0.2) * (cellSize)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>▶</text>
+        <text x={(0 + 0.5) * (cellSize)} y={(5 - 0.2) * (cellSize)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>{props.field.calc_bottom() + ' hit'}</text>
+        <text x={(0 + 0.9) * (cellSize)} y={(5 - 0.2) * (cellSize)}
+            textAnchor="middle" dominantBaseline="central" fill="#e6edff" fontSize={10}>▶</text>
         {
 
             <text x={(4 + 0.5) * (cellSize)} y={(4 + 0.5) * (cellSize)}
