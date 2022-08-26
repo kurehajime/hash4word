5a8179b91496ea95a76260da1151bec6944d52ba
kurehajime <xiidec@gmail.com>
2022-08-27 00:30:34 +0900

    説明を追加

--- src/compornents/Share/LogoElement.tsx
+++ src/compornents/Share/LogoElement.tsx
@@ -1,4 +1,11 @@
 import './LogoElement.css';
 export default function LogoElement() {
-    return (<div className="logo"><a href="https://github.com/kurehajime/hash4word">HASH4WORD</a> </div>)
+    return (
+        <div className="logoBox">
+            <div className="logo">
+                <a href="https://github.com/kurehajime/hash4word">HASH4WORD</a>
+            </div>
+            <p>Let&apos;s make 4 words<br /> by replacing letters.</p>
+        </div>
+    )
 }
