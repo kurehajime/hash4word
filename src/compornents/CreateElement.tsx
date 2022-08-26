825861a5c3738a6a8779839680ac6535e42acccd
kurehajime <xiidec@gmail.com>
2022-08-26 18:07:03 +0900

    英語は英語で初期化

--- src/compornents/CreateElement.tsx
+++ src/compornents/CreateElement.tsx
@@ -2,21 +2,32 @@ import { useEffect, useState } from "react"
 import { Field } from "../models/Field"
 import { InputField } from "../models/InputField"
 import word_japanese2048 from '../assets/japanese2048.json'
+import word_english2048 from '../assets/english2048.json'
 import InputFieldElement from "./Create/InputFieldElement"
-import { Point } from "../models/Point";
 import ShareTextElement from "./Create/ShareTextElement"
 import LogoElement from "./Share/LogoElement"
 
 type Props = {
     cellSize: number
+    init_mode: number
 }
 export default function CreateElement(props: Props) {
     const [inputField, setInputField] = useState<InputField | null>(null)
 
     useEffect(() => {
         const runes_hiragana = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎくげこざじずぜぞだぢづでどばびぶべぼぱぴぷぺぽっぁぃぅぇぉ'.split('')
-        const field = Field.createField(runes_hiragana, word_japanese2048, 4, undefined)
-        setInputField(InputField.createInputFieldbySeed(field.Seed))
+        const runes_english = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+        let field: Field
+        switch (props.init_mode) {
+            case 1:
+                field = Field.createField(runes_hiragana, word_japanese2048, 4, undefined)
+                setInputField(InputField.createInputFieldbySeed(field.Seed))
+                break
+            case 2:
+                field = Field.createField(runes_english, word_english2048, 4, undefined)
+                setInputField(InputField.createInputFieldbySeed(field.Seed))
+                break
+        }
     }, [])
 
     return (
