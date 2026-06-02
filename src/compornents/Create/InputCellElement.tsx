2e8225eb321b97ede918175b1db497eebb379190
kurehajime <xiidec@gmail.com>
2026-06-02 21:50:56 +0900

    白黒調にリニューアル

--- src/compornents/Create/InputCellElement.tsx
+++ src/compornents/Create/InputCellElement.tsx
@@ -11,12 +11,13 @@ export default function InputCellElement(props: Props) {
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         props.edit(props.cell.x, props.cell.y, e.target.value)
     }
+    const isCross = (props.cell.x === 3 || props.cell.x === 5) && (props.cell.y === 3 || props.cell.y === 5)
     return (
         props.cell.enabled ? <input type="text" className="inputCell"
             style={{
                 height: props.cellSize - 2, width: props.cellSize - 2, position: "absolute", left: props.x, top: props.y,
-                backgroundColor: props.cell.fixed ? '#4463b3' : '#e6edff',
-                color: props.cell.fixed ? '#ffffff' : "#202f55"
+                backgroundColor: isCross ? '#000000' : '#ffffff',
+                color: isCross ? '#ffffff' : "#000000"
             }}
             maxLength={1}
             value={props.cell.Rune}
--- src/compornents/Game/CellElement.tsx
+++ src/compornents/Game/CellElement.tsx
@@ -15,33 +15,36 @@ export default function CellElement(props: Props) {
     const cellSize2 = props.selected ? cellSize * 1.05 : cellSize
     const x = props.selected ? props.mouseX : props.x
     const y = props.selected ? props.mouseY : props.y
+    const isCross = (props.cell.x === 3 || props.cell.x === 5) && (props.cell.y === 3 || props.cell.y === 5)
+    const foregroundColor = isCross ? "#ffffff" : "#000000"
+    const backgroundColor = isCross ? "#000000" : "#ffffff"
     return (
         props.cell.enabled ? <g>
             <rect x={props.x} y={props.y} width={cellSize} height={cellSize}
-                fill='#131c33' className="" />
+                fill={props.selected ? "#333333" : "#000000"} className="" />
             <g>
                 <rect x={x + 1} y={y + 1} width={(cellSize2) - 2} height={(cellSize2) - 2}
-                    fill={props.cell.fixed ? '#4463b3' : '#e6edff'}
+                    fill={backgroundColor}
                     className={(props.selected ? "selected" : "easeIn") + " " + (props.cell.fixed ? "fixed" : "")} />
                 <text x={x + cellSize / 2} y={y + cellSize / 2}
                     className={(props.selected ? "selected" : "easeIn") + " " + (props.cell.fixed ? "fixed" : "")}
                     textAnchor="middle" dominantBaseline="central"
-                    fill={props.cell.fixed ? "#ffffff" : "#202f55"}
-                    stroke={props.cell.fixed ? "#ffffff" : "#202f55"}
+                    fill={foregroundColor}
+                    stroke={foregroundColor}
                     fontSize={props.selected ? "42px" : "40px"}
                     fontFamily="Helvetica Neue, Arial"
                 >{props.cell.Rune}</text>
 
-                <line x1={x + (cellSize2) - 0} y1={y + 0} x2={x + (cellSize2) - 0} y2={y + (cellSize2)} stroke="#202f55" strokeWidth="3" />
-                <line x1={x + (cellSize2) - 0} y1={y + (cellSize2) - 0} x2={x + 0} y2={y + (cellSize2) - 0} stroke="#202f55" strokeWidth="3" />
+                <line x1={x + (cellSize2) - 0} y1={y + 0} x2={x + (cellSize2) - 0} y2={y + (cellSize2)} stroke="#000000" strokeWidth="3" />
+                <line x1={x + (cellSize2) - 0} y1={y + (cellSize2) - 0} x2={x + 0} y2={y + (cellSize2) - 0} stroke="#000000" strokeWidth="3" />
             </g>
             {
                 (props.selected && props.touched) ? <g>
                     <text x={x + cellSize / 2 - cellSize} y={y + cellSize / 2 - cellSize}
                         className={(props.selected ? "selected" : "easeIn") + " " + (props.cell.fixed ? "fixed" : "")}
                         textAnchor="middle" dominantBaseline="central"
-                        fill={"#f0f8ff"}
-                        stroke={"#00294d"}
+                        fill={"#000000"}
+                        stroke={"#000000"}
                         fontSize={"100px"}
                         fontFamily="Helvetica Neue, Arial"
                         fontWeight="bold"
