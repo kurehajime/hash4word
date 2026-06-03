ac87db20de781b268f264e5f7afb8d1a13a91d64
kurehajime <xiidec@gmail.com>
2026-06-03 21:03:20 +0900

    矢印置き換え

--- src/compornents/Game/ScoreElement.tsx
+++ src/compornents/Game/ScoreElement.tsx
@@ -1,4 +1,5 @@
 import { Field } from "../../models/Field"
+import arrowRightIcon from "../../assets/arrow_right.svg"
 import "./ScoreElement.css"
 
 type Props = {
@@ -17,24 +18,35 @@ export default function ScoreElement(props: Props) {
         fontFamily: "Noto Sans JP, Helvetica Neue, Arial, sans-serif",
         fontWeight: 900,
     }
+    const arrowIcon = (x: number, y: number, rotation = 0) => {
+        const iconSize = 20
+        return <image
+            href={arrowRightIcon}
+            x={x - iconSize / 2}
+            y={y - iconSize / 2}
+            width={iconSize}
+            height={iconSize}
+            transform={rotation === 0 ? undefined : `rotate(${rotation} ${x} ${y})`}
+        />
+    }
     const leftHit = props.field.calc_left() + ' HIT'
     const rightHit = props.field.calc_right() + ' HIT'
     const topHit = props.field.calc_top() + ' HIT'
     const bottomHit = props.field.calc_bottom() + ' HIT'
     const leftX = (3 + 1.4) * cellSize
     const rightX = (5 + 1.4) * cellSize
-    const topArrowY = (0 + 1.5) * (cellSize / 2)
+    const topArrowY = (0 + 1.65) * (cellSize / 2)
     const topHitY = (0 + 1) * (cellSize / 2)
     const sideTopY = (3 - 0.2) * cellSize
     const sideBottomY = (5 - 0.2) * cellSize
     return (<g width={FieldSize} height={FieldSize}>
         <text x={leftX} y={topHitY} {...textProps}>{leftHit}</text>
-        <text x={leftX} y={topArrowY} {...textProps}>▼</text>
+        {arrowIcon(leftX, topArrowY, 90)}
         <text x={rightX} y={topHitY} {...textProps}>{rightHit}</text>
-        <text x={rightX} y={topArrowY} {...textProps}>▼</text>
+        {arrowIcon(rightX, topArrowY, 90)}
         <text x={(0 + 0.5) * cellSize} y={sideTopY} {...textProps}>{topHit}</text>
-        <text x={(0 + 0.9) * cellSize} y={sideTopY} {...textProps}>▶︎</text>
+        {arrowIcon((0 + 1.0) * cellSize, sideTopY)}
         <text x={(0 + 0.5) * cellSize} y={sideBottomY} {...textProps}>{bottomHit}</text>
-        <text x={(0 + 0.9) * cellSize} y={sideBottomY} {...textProps}>▶︎</text>
+        {arrowIcon((0 + 1.0) * cellSize, sideBottomY)}
     </g>)
 }
