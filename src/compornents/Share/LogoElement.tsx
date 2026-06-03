9e35b5f3f03e727c5b1c477869c4a940c53b4e81
kurehajime <xiidec@gmail.com>
2026-06-03 23:17:25 +0900

    説明文を強調

--- src/compornents/Share/LogoElement.tsx
+++ src/compornents/Share/LogoElement.tsx
@@ -20,7 +20,7 @@ export default function LogoElement(props: Props) {
                 <RotatedButtonText lines={[
                     props.create ? t('create_description1') : t('description1'),
                     props.create ? t('create_description2') : t('description2'),
-                ]} />
+                ]} animate />
             </p>
         </div>
     )
