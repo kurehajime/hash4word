b764307595e722c786e5aaffb8cb984fa75c6d22
kurehajime <xiidec@gmail.com>
2026-06-13 22:53:11 +0900

    ロゴにもノイズ乗っける

--- src/compornents/Share/LogoElement.tsx
+++ src/compornents/Share/LogoElement.tsx
@@ -16,8 +16,37 @@ export default function LogoElement(props: Props) {
     return (
         <div className={`logoBox ${languageClass}`}>
             <div className="logo">
-                <a href="https://github.com/kurehajime">
-                    <img className="titleImage" src={titleImage} alt="YONTANGO" />
+                <a href="https://github.com/kurehajime" aria-label="YONTANGO">
+                    <svg className="titleImage" viewBox="0 0 160 80" role="img" aria-label="YONTANGO">
+                        <title>YONTANGO</title>
+                        <defs>
+                            <filter id="title-image-grunge" x="-20%" y="-20%" width="140%" height="140%">
+                                <feTurbulence type="fractalNoise" baseFrequency="0.4" numOctaves="3" seed="8" result="noise" />
+                                <feColorMatrix
+                                    in="noise"
+                                    type="matrix"
+                                    values="0 0 0 0 0
+                                            0 0 0 0 0
+                                            0 0 0 0 0
+                                            1 0 0 0 0"
+                                    result="noiseAlpha"
+                                />
+                                <feComponentTransfer in="noiseAlpha" result="grungeAlpha">
+                                    <feFuncA type="discrete" tableValues="0 0 1 1 1 1 1 1" />
+                                </feComponentTransfer>
+                                <feComposite in="SourceGraphic" in2="grungeAlpha" operator="in" />
+                            </filter>
+                        </defs>
+                        <image
+                            href={titleImage}
+                            x={0}
+                            y={0}
+                            width={160}
+                            height={80}
+                            preserveAspectRatio="xMidYMid meet"
+                            filter="url(#title-image-grunge)"
+                        />
+                    </svg>
                 </a>
             </div>
             <p>
